995ce8f952b342f7649ddd560f99bc3484963c0d
Ramon Moura <ramon.moura@proluno.com.br>
2021-09-13 12:57:41 -0300

    Correção na configuração dos Paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ["dist"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["dist", "node_modules"],
   moduleNameMapper: {
     "@core/(.*)": "<rootDir>/src/core/$1",
     "@useCases/(.*)": "<rootDir>/src/core/useCases/$1",
     "@adapters/(.*)": "<rootDir>/src/adapters/$1",
     "@infrastructure/(.*)": "<rootDir>/src/infrastructure/$1",
-    "@exceptions/(.*)": "<rootDir>/src/exceptions/$1"
-  }
+    "@exceptions/(.*)": "<rootDir>/src/exceptions/$1",
+  },
 };
